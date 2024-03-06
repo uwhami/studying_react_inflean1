82700862cf6d24126f9d40dc6f020d6eaeb42bbc
dajeong <uwhami@gmail.com>
2024-03-06 23:44:38 +0900

    chapter_13

---
+++ my-app/src/chapter_13/ProfileCard.jsx
@@ -0,0 +1,15 @@
+import Card from "./Card";
+
+function ProfileCard(props) {
+  return (
+    <Card title="DJ Kim" backgroundColor="#4ea04e">
+      <p>안녕하세요, 다정이에요.</p>
+      <p>
+        저는 리액트를 사용해서 개발하고 있습니다. 졸리네요ㅠㅠ 얼릉 자고
+        싶어요..
+      </p>
+    </Card>
+  );
+}
+
+export default ProfileCard;
